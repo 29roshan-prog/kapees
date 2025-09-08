2d2da1010ee849196bc7f78dd4be80094c3e3b00
Roshan Prabhu <roshanprabhu05@gmail.com>
2025-09-08 19:57:58 +0530

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
-app.use(cors());
-app.use(express.static('public'));
-
-app.listen(5000, () => console.log('Server running on http://localhost:5000'));
